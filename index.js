3d95d9d76e1cf15be8223998368fe59a5a1c2065
tanyaleepr <tanyaleepr@gmail.com>
2022-02-14 21:45:36 -0600

    added inquirer, axios, generator for readme, and util

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ async function init() {
         const generateContent = generateReadme(answers);
         // Write new README.md to dist directory
         await writeFileAsync('./dist/README.md', generateContent);
-        console.log('✔️  Successfully wrote to README.md');
+        console.log('Successfully wrote to README.md');
     }   catch(err) {
         console.log(err);
     }
